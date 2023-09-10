fcda8fa3294900e4152983ed054b865f0e1d42f2
ulimy <ulim130@gmail.com>
2023-09-10 18:16:25 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] dot) {
+        if (dot[0] > 0 && dot[1] > 0) {
+            return 1;
+        }
+        if (dot[0] < 0 && dot[1] > 0) {
+            return 2;
+        }
+        if (dot[0] < 0 && dot[1] < 0) {
+            return 3;
+        }
+        return 4;
+    }
+}
