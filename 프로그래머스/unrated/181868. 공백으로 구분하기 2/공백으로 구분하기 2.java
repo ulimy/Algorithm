25a89851cc35f0d7ac261e1151318a02042934fb
ulimy <ulim130@gmail.com>
2023-10-06 00:49:31 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.07 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,11 +1,5 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 class Solution {
-    public List<String> solution(String my_string) {
-        return Arrays.stream(my_string.split(" "))
-            .filter(str -> str.length() > 0)
-            .collect(Collectors.toList());
+    public String[] solution(String my_string) {
+        return my_string.trim().split("[ ]+");
     }
 }
