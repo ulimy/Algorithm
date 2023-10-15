f8f2b08e31a18f2c24d1c53c78489be282bd911c
ulimy <ulim130@gmail.com>
2023-10-16 01:37:11 +0900

    [level unrated] Title: 배열 만들기 3, Time: 6.50 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,10 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        return Arrays.stream(intervals)
-            .map(interval -> Arrays.copyOfRange(arr, interval[0], interval[1] + 1))
-            .flatMapToInt(i -> Arrays.stream(i))
-            .toArray();
+        int[] answer = {};
+        return answer;
     }
 }
