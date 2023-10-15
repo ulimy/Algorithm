1bf6ee2bf51f6b8b8ec3c4aacd68d35b2b6b310c
ulimy <ulim130@gmail.com>
2023-10-16 02:09:42 +0900

    [level unrated] Title: 합성수 찾기, Time: 2.68 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.java
@@ -1,17 +1,6 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int n) {
-        return (int)IntStream.rangeClosed(4, n)
-            .filter(this::isComposite)
-            .count();
-    }
-
-    private boolean isComposite(int num) {
-        int count = (int)IntStream.rangeClosed(1, num)
-            .filter(target -> num % target == 0)
-            .count();
-
-        return count >= 3;
+        int answer = 0;
+        return answer;
     }
 }
