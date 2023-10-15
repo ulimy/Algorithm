2b51c245a8ed35583a9b062646b503d62ca8ed05
ulimy <ulim130@gmail.com>
2023-10-16 01:58:06 +0900

    [level unrated] Title: 9로 나눈 나머지, Time: 1.30 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -4,7 +4,6 @@ class Solution {
     public int solution(String number) {
         return Arrays.stream(number.split(""))
             .mapToInt(Integer::valueOf)
-            .reduce((a, b) -> a + b)
-            .getAsInt() % 9;
+            .sum() % 9;
     }
 }
