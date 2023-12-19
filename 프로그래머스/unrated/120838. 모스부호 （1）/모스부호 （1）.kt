3d341b47c2ed82680a5852650e22b24f1d75de8f
ulimy <ulim130@gmail.com>
2023-12-20 01:11:52 +0900

    [level unrated] Title: 모스부호 (1), Time: 15.53 ms, Memory: 60.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun solution(letter: String): String {
+        val morse = mapOf(
+            ".-" to "a", "-..." to "b", "-.-." to "c", "-.." to "d", "." to "e", "..-." to "f",
+            "--." to "g", "...." to "h", ".." to "i", ".---" to "j", "-.-" to "k", ".-.." to "l",
+            "--" to "m", "-." to "n", "---" to "o", ".--." to "p", "--.-" to "q", ".-." to "r",
+            "..." to "s", "-" to "t", "..-" to "u", "...-" to "v", ".--" to "w", "-..-" to "x",
+            "-.--" to "y", "--.." to "z"
+
+        )
+
+        return letter.split(" ")
+            .map { letter -> morse[letter] }
+            .reduce { i, j -> i + j }
+            .toString()
+    }
+}
