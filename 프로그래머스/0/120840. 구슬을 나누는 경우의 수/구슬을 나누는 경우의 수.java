fa2dfac39535ea5e44c3a978b4850621f3d82826
ulimy <ulim130@gmail.com>
2024-02-19 23:32:35 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int balls, int share) {
+        long answer = 1;
+
+        for (int i = 1; i <= share; i++) {
+            answer *= (balls - i + 1);
+            answer /= i;
+        }
+
+        return (int)answer;
+    }
+}
