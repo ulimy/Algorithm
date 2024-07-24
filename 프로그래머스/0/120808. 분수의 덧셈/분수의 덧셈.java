981bd6682eef013bdf91d5a722c2ba6eeef028fe
ulimy <ulim130@gmail.com>
2024-07-24 22:23:03 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int denom = denom1 * denom2;
+        int numer = (numer1 * denom2) + (numer2 * denom1);
+        int gcd = gcd(denom, numer);
+
+        return new int[] {numer / gcd, denom / gcd};
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
